495db7cbcaf902d8396c9225b807d4ec3b013a39
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-09-24 02:00:55 +0100

    Update Navbar.jsx

--- src/components/General/Navbar.jsx
+++ src/components/General/Navbar.jsx
@@ -1,38 +1,42 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
-    let paths = [
-        {
-            path: '/newer',
-            name: 'Posts Recentes'
-        },
-    ]
+  let paths = [
+    {
+      path: "/newer",
+      name: "Posts Recentes",
+    },
+    {
+      path: "https://devscafe.vercel.app/",
+      name: "Dev's Café",
+    }
+  ];
 
-    return (
-        <nav id={"navbar"}>
-            <div>
-                <Link href={"/"}>
-                    <Image
-                        src="/logo.png"
-                        alt="Logo"
-                        width={50}
-                        height={50}
-                        id={"navbar-logo"}
-                    />
-                </Link>
-            </div>
-            <div>
-                <ul id={"navbar-list"}>
-                    {paths.map((path) => (
-                        <li key={path.path}>
-                            <Link href={path.path}>
-                                <h2>{path.name}</h2>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </nav>
-    )
+  return (
+    <nav id={"navbar"}>
+      <div>
+        <Link href={"/"}>
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={50}
+            height={50}
+            id={"navbar-logo"}
+          />
+        </Link>
+      </div>
+      <div>
+        <ul id={"navbar-list"}>
+          {paths.map((path) => (
+            <li key={path.path}>
+              <Link href={path.path}>
+                <h2>{path.name}</h2>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 }
