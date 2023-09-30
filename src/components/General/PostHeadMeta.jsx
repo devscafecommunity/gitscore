b337bc474e12b7f5efee59eeae08c361f58c201c
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-09-30 22:56:32 +0100

    Update PostHeadMeta.jsx

--- src/components/General/PostHeadMeta.jsx
+++ src/components/General/PostHeadMeta.jsx
@@ -5,15 +5,7 @@ export default function PostHeadMeta({ frontMatter, slug }) {
     let preview = `https://cafe-content.vercel.app/api/blog/postpreview/${slug}`;
     return (
         <Head>
-            <title>{frontMatter.title}</title>
-            <meta name="description" content={frontMatter.description} />
-            {/* <meta property="og:type" content='' />  type big image*/}
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content={frontMatter.title} />
-            <meta property="og:description" content={frontMatter.description} />
             <meta property="og:image" content={preview} />
-            <meta property="og:article:published_time" content={frontMatter.date} />
-            <meta property="og:article:author" content={frontMatter.author} />
         </Head>
     );
 }
