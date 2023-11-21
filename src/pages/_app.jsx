d80a389dd1d5ab12b996407eecc13557b1477531
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-11-21 15:59:56 +0000

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,11 +7,22 @@ import '../styles/globals.css';
 // Components
 import Navbar from '../components/General/Navbar';
 
+const meta = {
+    favicon: "/logo.png",
+    title: "Blog do Dev's Café",
+    description: "Blog oficial da comunidade.",
+}
+
 export default class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props;
         return (
             <div>
+                {/* Load meta */}
+                <title>{meta.title}</title>
+                <link rel="icon" href={meta.favicon} />
+                <meta name="description" content={meta.description} />
+                {/* Load components */}
                 <Navbar />
                 <span style={{margin: "10",}}></span>
                 <Component {...pageProps} />
