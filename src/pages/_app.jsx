99de21522feeac4e6f69749f7fe894649c095309
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-11-21 16:10:29 +0000

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import App from 'next/app';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 // Components
 import Navbar from '../components/General/Navbar';
@@ -21,6 +22,7 @@ export default class MyApp extends App {
         return (
             <div>
                 {/* Load meta */}
+                <Head>
                 <title>{meta.title}</title>
                 <link rel="icon" href={meta.favicon} />
                 <meta name="description" content={meta.description} />
@@ -28,6 +30,13 @@ export default class MyApp extends App {
                 <meta property="og:url" content={meta.url} />
                 <meta property="og:title" content={meta.title} />
                 <meta property="og:description" content={meta.description} />
+                <meta property="og:image" content={meta.image} />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content={meta.url} />
+                <meta property="twitter:title" content={meta.title} />
+                <meta property="twitter:description" content={meta.description} />
+                <meta property="twitter:image" content={meta.image} />
+                </Head>
                 {/* Load components */}
                 <Navbar />
                 <span style={{margin: "10",}}></span>
