e2baa2c4d1665e1eb54039913ac7017b0f466176
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-11-21 17:26:59 +0000

    Commit

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import App from 'next/app';
 import '../styles/globals.css';
-import Head from 'next/head';
 
 // Components
 import Navbar from '../components/General/Navbar';
@@ -21,23 +20,6 @@ export default class MyApp extends App {
         const { Component, pageProps } = this.props;
         return (
             <div>
-                {/* Load meta */}
-                <Head>
-                <title>{meta.title}</title>
-                <link rel="icon" href={meta.favicon} />
-                <meta name="description" content={meta.description} />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content={meta.url} />
-                <meta property="og:title" content={meta.title} />
-                <meta property="og:description" content={meta.description} />
-                <meta property="og:image" content={meta.image} />
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content={meta.url} />
-                <meta property="twitter:title" content={meta.title} />
-                <meta property="twitter:description" content={meta.description} />
-                <meta property="twitter:image" content={meta.image} />
-                </Head>
-                {/* Load components */}
                 <Navbar />
                 <span style={{margin: "10",}}></span>
                 <Component {...pageProps} />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,40 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-import Link from 'next/link'
-import path from 'path'
-import { postFilePaths, POSTS_PATH } from '../utils/mdxUtils'
+import fs from "fs";
+import matter from "gray-matter";
+import Link from "next/link";
+import path from "path";
+import { postFilePaths, POSTS_PATH } from "../utils/mdxUtils";
+import Head from "next/head";
 
 // Components
-import PostsList from '../components/Index/PostsList'
-import Header from '../components/Index/Header'
+import PostsList from "../components/Index/PostsList";
+import Header from "../components/Index/Header";
 
 export default function Index({ posts }) {
   return (
     <div
-    style={{
-      flex: 1,
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-    }}
+      style={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
     >
+      <Head>
+        <title>{meta.title}</title>
+        <link rel="icon" href={meta.favicon} />
+        <meta name="description" content={meta.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={meta.url} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={meta.image} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={meta.url} />
+        <meta property="twitter:title" content={meta.title} />
+        <meta property="twitter:description" content={meta.description} />
+        <meta property="twitter:image" content={meta.image} />
+      </Head>
       <div
         style={{
           display: "flex",
@@ -46,20 +62,20 @@ export default function Index({ posts }) {
         <PostsList posts={posts} />
       </div>
     </div>
-  )
+  );
 }
 
 export function getStaticProps() {
   const posts = postFilePaths().map((filePath) => {
-    const source = fs.readFileSync(path.join(POSTS_PATH, filePath))
-    const { content, data } = matter(source)
+    const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
+    const { content, data } = matter(source);
 
     return {
       content,
       data,
       filePath,
-    }
-  })
+    };
+  });
 
-  return { props: { posts } }
+  return { props: { posts } };
 }
