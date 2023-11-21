fd37df4670e5d7a0a51ca5b504b6c3c64ca60541
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-11-21 15:42:40 +0000

    Update PostHeadMeta.jsx

--- src/components/General/PostHeadMeta.jsx
+++ src/components/General/PostHeadMeta.jsx
@@ -4,19 +4,22 @@ import Head from "next/head";
 export default function PostHeadMeta({ frontMatter, slug }) {
     let preview = `${process.env.SEARCH_ENGINE}/api/blog/postpreview/${slug}`;
     return (
+        // Large Image description
+        // discord, twitter, facebook, linkedin, telegram, whatsapp
         <Head>
+            <title>{frontMatter.title}</title>
+            <meta name="title" content={frontMatter.title} />
+            <meta name="description" content={frontMatter.description} />
             <meta property="og:type" content="website" />
             <meta property="og:url" content={preview} />
             <meta property="og:title" content={frontMatter.title} />
             <meta property="og:description" content={frontMatter.description} />
-            <meta property="og:image" content={preview} />
+            <meta property="og:image" content={frontMatter.cover} />
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content={preview} />
             <meta property="twitter:title" content={frontMatter.title} />
             <meta property="twitter:description" content={frontMatter.description} />
-            <meta property="twitter:image" content={preview} />
-            {/* <meta property="og:image" content={preview} /> */}
-            {/* <meta property="og:image" content={frontMatter.cover} /> */}
+            <meta property="twitter:image" content={frontMatter.cover} />
         </Head>
     );
 }
