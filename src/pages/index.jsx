6048736e64540536393478622e85fd8b969183c6
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-11-21 17:40:34 +0000

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,20 +27,6 @@ export default function Index({ posts }) {
         justifyContent: "center",
       }}
     >
-      <Head>
-        <title>{meta.title}</title>
-        <meta name="description" content={meta.description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={meta.url} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:image" content={meta.image} />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={meta.url} />
-        <meta property="twitter:title" content={meta.title} />
-        <meta property="twitter:description" content={meta.description} />
-        <meta property="twitter:image" content={meta.image} />
-      </Head>
       <div
         style={{
           display: "flex",
