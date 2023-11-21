0def36cd429ee80a51c88f71afffa9ce1ff6d111
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-11-21 16:04:05 +0000

    Update PostHeadMeta.jsx

--- src/components/General/PostHeadMeta.jsx
+++ src/components/General/PostHeadMeta.jsx
@@ -14,12 +14,12 @@ export default function PostHeadMeta({ frontMatter, slug }) {
             <meta property="og:url" content={preview} />
             <meta property="og:title" content={frontMatter.title} />
             <meta property="og:description" content={frontMatter.description} />
-            <meta property="og:image" content={frontMatter.cover} />
+            <meta property="og:image" content={preview} />
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content={preview} />
             <meta property="twitter:title" content={frontMatter.title} />
             <meta property="twitter:description" content={frontMatter.description} />
-            <meta property="twitter:image" content={frontMatter.cover} />
+            <meta property="twitter:image" content={preview} />
         </Head>
     );
 }
